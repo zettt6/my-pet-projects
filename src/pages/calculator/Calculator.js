73eed49c35c5536fd52d686fc7cfd9b2a4b4f2b1
zettt6 <n.diana9937@gmail.com>
2021-09-20 22:56:22 +0300

    new features in calculator

--- src/pages/calculator/Calculator.js
+++ src/pages/calculator/Calculator.js
@@ -1,54 +1,126 @@
+import { useState } from 'react/cjs/react.development'
 import './Calculator.css'
 
 function Calculator() {
+   const [nums, setNums] = useState([])
+   const [operators, setOperators] = useState([])
+   const [inputText, setInputText] = useState(0)
+
+   const [result, setResult] = useState('')
+
+   let btnData = [
+      { value: 'AC', type: 'clean', className: 'btn grey-highligh' },
+      { value: '+/-', type: 'posOrNeg', className: 'btn grey-highligh' },
+      { value: '%', type: 'procent', className: 'btn grey-highligh' },
+      { value: '÷', type: 'divide', className: 'btn orange-highligh' },
+
+      { value: '7', type: 'num', className: 'btn' },
+      { value: '8', type: 'num', className: 'btn' },
+      { value: '9', type: 'num', className: 'btn' },
+
+      { value: '×', type: 'multiply', className: 'btn orange-highligh' },
+
+      { value: '4', type: 'num', className: 'btn' },
+      { value: '5', type: 'num', className: 'btn' },
+      { value: '6', type: 'num', className: 'btn' },
+
+      { value: '-', type: 'minus', className: 'btn orange-highligh' },
+
+      { value: '1', type: 'num', className: 'btn' },
+      { value: '2', type: 'num', className: 'btn' },
+      { value: '3', type: 'num', className: 'btn' },
+
+      { value: '+', type: 'plus', className: 'btn orange-highligh' },
+
+      { value: '0', type: 'num', className: 'btn zero' },
+
+      { value: ',', type: 'decimal', className: 'btn' },
+      { value: '=', type: 'equel', className: 'btn orange-highligh' },
+   ]
+
+   // console.log(nums)
+   // console.log(operators)
+   // console.log('result = ' + result)
+   // console.log('+' + result.charAt(0))
+
+   function typeOfBtn(btn) {
+      switch (btn.type) {
+         case 'num':
+            setInputText(btn.value)
+            setNums([...nums, btn.value])
+            break
+         case 'equel':
+            calculate()
+            break
+         case 'clean':
+            setResult('')
+            setNums([])
+            setOperators([])
+            setInputText(0)
+            break
+         default:
+            setOperators([...operators, btn.value])
+      }
+   }
+
+   function calculate() {
+      let lastOperator = operators[operators.length - 1]
+      let lastNum = +nums[nums.length - 1]
+      let tempRes = result
+
+      if (lastOperator === '') {
+         // setNums([...nums, nums[nums.length - 1] + 'hey'])
+         console.log('hi')
+      } else if (lastOperator === '+') {
+         tempRes = +nums[nums.length - 2] + lastNum
+      } else if (lastOperator === '-') {
+         tempRes = +nums[nums.length - 2] - lastNum
+      } else if (lastOperator === '×') {
+         tempRes = +nums[nums.length - 2] * lastNum
+      } else if (lastOperator === '÷') {
+         tempRes = +nums[nums.length - 2] / lastNum
+      } else if (lastOperator === '%') {
+         tempRes = (+nums[nums.length - 2] * lastNum) / 100
+      }
+      // else if (
+      //    lastOperator === '+/-' &&
+      //    lastNum.toString().charAt(-1) === ''
+      // ) {
+      //    tempRes = '-' + lastNum
+      // }
+      // else if (
+      //    lastOperator === '+/-' &&
+      //    lastNum.toString().charAt(-1) === '-'
+      // ) {
+      //    tempRes = '' + tempRes
+      // }
+      else return setInputText(tempRes)
+
+      setNums([...nums, tempRes])
+      setResult(`${tempRes}`)
+      setInputText(tempRes)
+      setOperators([''])
+   }
+
    return (
       <div className="Calculator">
-         <div class="calc-wrapper background">
-            <div className="input-area">
-               <input className="calc-input" type="text" />
+         <div className="calc-wrapper">
+            <div>
+               <input className="input" type="text" value={inputText} />
             </div>
+
             <div className="btns-container">
-               <div className="left-symbols-container">
-                  <div className="first-row">
-                     <button className="calc-btn">AC</button>
-                     <button className="calc-btn">+/-</button>
-                     <button className="calc-btn">%</button>
-                  </div>
-                  <br />
-                  <div className="main-symbols">
-                     <button className="calc-btn">7</button>
-                     <button className="calc-btn">8</button>
-                     <button className="calc-btn">9</button>
-                  </div>
-                  <br />
-                  <div className="main-symbols">
-                     <button className="calc-btn">4</button>
-                     <button className="calc-btn">5</button>
-                     <button className="calc-btn">6</button>
-                  </div>
-                  <br />
-                  <div class="main-symbols">
-                     <button className="calc-btn">1</button>
-                     <button className="calc-btn">2</button>
-                     <button className="calc-btn">3</button>
-                  </div>
-                  <br />
-                  <div className="main-symbols">
-                     <button class="null calc-btn">0</button>
-                     <button className="calc-btn">,</button>
-                  </div>
-               </div>
-               <div className="right-symbols-container">
-                  <button className="calc-btn">÷</button>
-                  <br />
-                  <button className="calc-btn">×</button>
-                  <br />
-                  <button className="calc-btn">-</button>
-                  <br />
-                  <button className="plus calc-btn">+</button>
-                  <br />
-                  <button className="calc-btn">=</button>
-               </div>
+               {btnData.map((btn, index) => {
+                  return (
+                     <button
+                        key={index.toString()}
+                        className={btn.className}
+                        onClick={() => typeOfBtn(btn)}
+                     >
+                        {btn.value}
+                     </button>
+                  )
+               })}
             </div>
          </div>
       </div>
@@ -56,3 +128,77 @@ function Calculator() {
 }
 
 export default Calculator
+
+// switch (btn.type) {
+//    case 'plus':
+//       console.log('plus')
+//       break
+//    case 'minus':
+//       console.log('minus')
+//       break
+//    case 'divide':
+//       console.log('divide')
+//       break
+//    case 'multiply':
+//       console.log('multiply')
+//       break
+//    case 'clean':
+//       console.log('clean')
+//       break
+//    case 'procent':
+//       console.log('procent')
+//       break
+//    case 'posOrNeg':
+//       console.log('posOrNeg')
+//       break
+//    case 'equel':
+//       console.log('equel')
+//       break
+//    case 'decimal':
+//       console.log('decimal')
+//       break
+//    default:
+//       console.log(btn.value)
+// }
+
+{
+   /* <button className="btn grey-highligh" id="clean">
+                  AC
+               </button>
+               <button className="btn grey-highligh" id="negOrPos">
+                  +/-
+               </button>
+               <button className="btn grey-highligh" id="procent">
+                  %
+               </button>
+               <button className="btn orange-highligh" id="div">
+                  ÷
+               </button>
+               <button className="btn">7</button>
+               <button className="btn">8</button>
+               <button className="btn">9</button>
+               <button className="btn orange-highligh" id="mult">
+                  ×
+               </button>
+               <button className="btn">4</button>
+               <button className="btn">5</button>
+               <button className="btn">6</button>
+               <button className="btn orange-highligh" id="minus">
+                  -
+               </button>
+               <button className="btn">1</button>
+               <button className="btn">2</button>
+               <button className="btn">3</button>
+               <button className="btn orange-highligh" id="plus">
+                  +
+               </button>
+               <button className="btn" id="zero">
+                  0
+               </button>
+               <button className="btn" id="dot">
+                  .
+               </button>
+               <button className="btn orange-highligh" id="equel">
+                  =
+               </button> */
+}
